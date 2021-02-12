b5be5d10003e7c53e8ec8b0a5996fd3ae8f27768
Esteban <esteban.ramirezrivera@gmail.com>
2021-02-11 18:10:16 -0600

    Typescript Jquery2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("jquery");
+var sweetalert2_1 = require("sweetalert2");
 console.log("Hola mundo");
 $(document).ready(function () {
     console.log('Lista y Pagina cargada');
@@ -9,7 +10,8 @@ jQuery(function () {
     console.log('Pagina lista y cargada');
     $("h1").text("acá estamos");
     $("p").text("Hola desde acá");
-    $("#botAlerta").on("click", function () {
-        alert("Hola mundo desde Typescript");
-    });
+});
+$("#botAlerta").on("click", function () {
+    //alert("Hola mundo desde Typescript");
+    sweetalert2_1.default.fire('Good job!', 'You clicked the button!', 'success');
 });
--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 import 'jquery';
+import swal from 'sweetalert2';
+
 
 console.log("Hola mundo");
 
 
 $(document).ready(function(){
     console.log('Lista y Pagina cargada');
-})
+});
 
 jQuery(function(){
     console.log('Pagina lista y cargada');
@@ -14,13 +16,15 @@ $("h1").text("acá estamos");
 
 $("p").text("Hola desde acá");
 
+
+
+
+
+
+
+});
+
 $("#botAlerta").on("click", function(){
-    alert("Hola mundo desde Typescript");
+    //alert("Hola mundo desde Typescript");
+    swal.fire('Good job!', 'You clicked the button!','success');
 });
-
-
-
-
-
-});
-
